fcdfa08887f96b688bf724107da0252c8e3a12fe
fiveseven-lambda <fiveseven.lambda@gmail.com>
2021-08-03 23:29:37 +0900

    Error のデストラクタを virtual に

--- src/error.h
+++ src/error.h
@@ -8,18 +8,21 @@ namespace error {
     class Error {
     public:
         void virtual print(const std::string &) = 0;
+        virtual ~Error() = default;
     };
     class InvalidCharacter: public Error {
         pos::Pos pos;
     public:
         InvalidCharacter(pos::Pos &&);
         void print(const std::string &);
+        virtual ~InvalidCharacter() = default;
     };
     class UnexpectedCharacter: public Error {
         pos::Range range;
     public:
         UnexpectedCharacter(pos::Range &&);
         void print(const std::string &);
+        virtual ~UnexpectedCharacter() = default;
     };
 }
 #endif
